617deb58b923458fd656853e1520742ed4eac8f7
JamesOngICL <73653114+JamesOngICL@users.noreply.github.com>
2023-01-27 14:53:54 +0000

    new temp

--- upload_fold.py
+++ upload_fold.py
@@ -1,22 +1,37 @@
 import time
 import smbus2
 
-si7021_ADD = 0x40 #Add the I2C bus address for the sensor here
-si7021_READ_TEMPERATURE = 0xE3 #Add the command to read temperature here
 
-bus = smbus2.SMBus(1)
+def conv_temperature(value):
+    """
+    Converts the temperature value to relevant integer format
+    """
+    temp = (175.72*value)/65536
+    temp -= 6
+    return temp
 
-#Set up a write transaction that sends the command to measure temperature
-cmd_meas_temp = smbus2.i2c_msg.write(si7021_ADD,[si7021_READ_TEMPERATURE])
+def read_temp():
+    si7021_ADD = 0x40 #Add the I2C bus address for the sensor here
+    si7021_READ_TEMPERATURE = 0xE3 #Add the command to read temperature here
 
-#Set up a read transaction that reads two bytes of data
-read_result = smbus2.i2c_msg.read(si7021_ADD,2)
+    bus = smbus2.SMBus(1)
 
-#Execute the two transactions with a small delay between them
-bus.i2c_rdwr(cmd_meas_temp)
-time.sleep(0.1)
-bus.i2c_rdwr(read_result)
+    #Set up a write transaction that sends the command to measure temperature
+    cmd_meas_temp = smbus2.i2c_msg.write(si7021_ADD,[si7021_READ_TEMPERATURE])
 
-#convert the result to an int
-temperature = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
-print(temperature)
+    #Set up a read transaction that reads two bytes of data
+    read_result = smbus2.i2c_msg.read(si7021_ADD,2)
+
+    #Execute the two transactions with a small delay between them
+    bus.i2c_rdwr(cmd_meas_temp)
+    time.sleep(0.1)
+    bus.i2c_rdwr(read_result)
+
+    #convert the result to an int
+    temperature = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
+    print("displayin temperature", conv_temperature(temperature))
+    pass
+
+
+
+
