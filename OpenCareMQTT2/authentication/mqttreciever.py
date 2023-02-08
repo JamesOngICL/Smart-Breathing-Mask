3b97e0031606020e97b08fb27c404d0ebf709ff6
matthew-setiawan <94852286+matthew-setiawan@users.noreply.github.com>
2023-02-08 14:24:30 +0000

    Latest Version

---
+++ OpenCareMQTT2/authentication/mqttreciever.py
@@ -0,0 +1,42 @@
+import requests 
+import paho.mqtt.client as mqtt
+import time
+import extfunctions
+import ast
+
+def updatereadingmqtt(message):
+      tmp = ast.literal_eval(message)
+      print(tmp)
+      for i in tmp:
+            n = i.split(":")
+            if len(n)>1:
+                  print("param:",tmp["address"],tmp[i],n[1])
+                  val = ""
+                  if n[1]=="temperaturereading":
+                        val = tmp[i]
+                  elif n[1]=="accelerometerreading":
+                        print("vals")
+                        print(float(tmp[i][0]))
+                        print(float(tmp[i][0]))
+                        print(float(tmp[i][0]))
+                        val = (float(tmp[i][0])**2 + float(tmp[i][1])**2 + float(tmp[i][2])**2)**0.5
+                  elif n[1]=="co2reading":
+                        val = tmp[i]
+                  elif n[1]=="heartratereading":
+                        val = tmp[i]
+                  extfunctions.updatereading(tmp["address"],n[1],val)
+
+def on_message(client, userdata, message):
+      print("received message: " ,str(message.payload.decode("utf-8")))
+      #Execute this upon recieving message
+      file = open("mqttmessages.txt","w")
+      file.write(str(message.payload.decode("utf-8")))
+      file.close()
+      updatereadingmqtt(str(message.payload.decode("utf-8")))
+
+mqttBroker ="test.mosquitto.org"
+client = mqtt.Client()
+client.connect(mqttBroker) 
+client.subscribe("IC.embedded/jomp/opencare")
+
+client.on_message=on_message 
